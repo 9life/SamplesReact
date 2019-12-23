e46ecb9fed20836d575702ed74e2730aeb39fa87
kokorina.on <kokorina.on@igit.spb.ru>
2019-12-23 16:01:49 +0300

    ++ Abstracting Form Input useState

--- src/todoList/TodoForm.js
+++ src/todoList/TodoForm.js
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
+import useInputState from './useInputState';
+
 
 const TodoForm = ({ saveTodo }) => {
-  const [value, setValue] = useState("");
+  //const [value, setValue] = useState("");
+  const {value, reset, onChange} = useInputState('');
+
   return (
     <form
       onSubmit={event => {
         event.preventDefault();
         saveTodo(value);
-        setValue('');
+        //setValue('');
+        reset();
       }}
     >
       <TextField
         variant="outlined"
         placeholder="Add todo"
         margin="normal"
-        onChange={event => {
-          setValue(event.target.value);
-        }}
+        onChange={onChange}
+        // {event => { setValue(event.target.value); }}
         value={value}
       />
     </form>
