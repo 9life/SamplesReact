0edf68aae8cb112143a01ddede4171fb65d1c4a4
kokorina.on <kokorina.on@igit.spb.ru>
2019-12-23 15:53:40 +0300

    ++ Abstracting Todos useState

--- src/todoList/todoApp.js
+++ src/todoList/todoApp.js
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom";
 import TodoForm from './TodoForm';
 import { Typography } from "@material-ui/core";
 import TodoList from './TodoList';
+import useTodoState from './useTodoState';
 
 
 const TodoApp =() => {
-    const [todos, setTodos] = useState([]);
+    //const [todos, setTodos] = useState([]);
+    const {todos, addTodo, deleteTodo} = useTodoState([]);
 
     return(
         <div>
@@ -17,12 +19,17 @@ const TodoApp =() => {
            <TodoForm saveTodo={(todoText) => {
                const trimmedText = todoText.trim();
 
-               if (trimmedText.length > 0) { setTodos([...todos, trimmedText]);}
+               if (trimmedText.length > 0) { 
+                   //setTodos([...todos, trimmedText]);
+                   addTodo(trimmedText);
+                }
            }}/>
-           <TodoList todos={todos} deleteTodos={(todoIndex) => {
-               const newTodos = todos.filter((_, index) => index !== todoIndex);
-               setTodos(newTodos);
-           }} />
+           <TodoList todos={todos} deleteTodos={deleteTodo}
+        //    {(todoIndex) => {
+        //        const newTodos = todos.filter((_, index) => index !== todoIndex);
+        //        setTodos(newTodos);
+        //    }} 
+           />
         </div>
     );
 }
