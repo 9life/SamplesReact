1950f4c30ec8ce0d602d41acc3977ec336934ecf
kokorina.on <kokorina.on@igit.spb.ru>
2019-12-26 17:38:38 +0300

    ++ weather icon

--- src/weather/Wapp.js
+++ src/weather/Wapp.js
@@ -3,18 +3,20 @@ import React, {useState, useEffect} from 'react';
 
 const Wapp = () => {
   // initialize state
-  const [city, setCity] = useState('Moscow')
+  const [city, setCity] = useState('Tokyo')
   const [data, setData] = useState({
       isFetching: false,
       data: null
   })
+
+  const ApiKey ="032291ed91f896b87b1d9c1f32a0de36";
 
   useEffect(() => {
     setData({
       isFetching: true
     })
 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=032291ed91f896b87b1d9c1f32a0de36`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${ApiKey}`)
       .then((response) => response.json())
       .then((responseJson) => {
         setData({
@@ -31,7 +33,7 @@ console.log(data.data);
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header"> */}
         <input type="text" placeholder="Tokyo"/>
         <button onClick={(e) => {
           if (! e.target.previousElementSibling.value) {
@@ -48,11 +50,12 @@ console.log(data.data);
                  <p>Temperature: {data.data.list[0].main.temp}</p>
                  <p>Wheather: {data.data.list[0].weather[0].main}</p>
                  <p>Wind: {data.data.list[0].wind.speed}</p>
+                 <img src={`http://openweathermap.org/img/w/${data.data.list[0].weather[0].icon}.png`}/>
                </div>
                
             )
         })()}
-      </header>
+      {/* </header> */}
     </div>
   );
 }
